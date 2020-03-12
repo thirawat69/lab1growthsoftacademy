692f1831aec560512d0ffc8678938dc0cac937e5
thirawat69 <thirawatp9@gmail.com>
2020-03-12 17:23:37 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,9 @@ module.exports = {
   httpport: 5713,
   httpsport: 35255,
   dbUrl:
-    "mongodb+srv://admin:Pond1234@growthacademycluster-oxmkq.gcp.mongodb.net/GROWTH_LAB1?retryWrites=true&w=majority",
+  //"mongodb+srv://admin:Pond1234@growthacademycluster-oxmkq.gcp.mongodb.net/GROWTH_LAB1?retryWrites=true&w=majority",
+  //"mongodb+srv://******:*******@growthacademycluster-oxmkq.gcp.mongodb.net/***********?retryWrites=true&w=majority",
+    "mongodb+srv://admin:1234566@growthacademycluster-oxmkq.gcp.mongodb.net/restaurant?retryWrites=true&w=majority",
   sessionSecret: "xxx",
   passportSecret: "Growth#Since2018",
   needReScrape: false
